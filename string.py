c762e74b7001e2ee13485317ec64bc9051d3ae6f
Ibinthomas <ibinthomas858@gmail.com>
2024-08-15 13:28:38 +0530

    edit

--- string.py
+++ string.py
@@ -1,5 +1,25 @@
-a="ibin"
-print(a[1])
-print(a[-2])
-print("python","\nfullstack")
+# a="ibin"
+# print(a[1])
+# print(a[-2])
+# print("python","\nfullstack")
 
+#string
+
+# a="possition"
+# # print(a[0:5])
+# # print(a[2:8])
+# # print(a[-2:])
+
+# a=input("enter a string:")
+# print(a[::-1])
+# print(a==a[::1])
+
+# string method
+a=input("enter a name")
+# print(a.upper())
+# print(a.lower())
+# print(a.isupper())
+# print(a.islower())
+# print(a.center(10))
+# print(a.count('i'))
+print(a.endswith('n'))
