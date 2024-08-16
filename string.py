072fddae52e6d46c1c4fd0eb8ca3ac7e5c1ad0c3
Ibinthomas <ibinthomas858@gmail.com>
2024-08-16 13:40:57 +0530

    edit

--- string.py
+++ string.py
@@ -14,12 +14,39 @@
 # print(a[::-1])
 # print(a==a[::1])
 
-# string method
-a=input("enter a name")
-# print(a.upper())
-# print(a.lower())
-# print(a.isupper())
-# print(a.islower())
-# print(a.center(10))
-# print(a.count('i'))
-print(a.endswith('n'))
+# # string method
+# a=input("enter a name")
+# # print(a.upper())
+# # print(a.lower())
+# # print(a.isupper())
+# # print(a.islower())
+# # print(a.center(10))
+# # print(a.count('i'))
+# print(a.endswith('n'))
+
+# a=int(input("enter a number"))
+# b=int(input("enter second number"))
+# if a>=b:
+#     print("largest is",a)
+# else:
+#     print("largest s",b)
+
+
+# age=int(input("enter age"))
+# if age>=18:
+#     print("eligible")
+# else: 
+#     print("not eligible")
+
+# n=int(input("enter a number"))
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")    
+
+s=input("enter a string")
+l=input("enter a letter")
+if l in s :
+    print("vissible")
+else:
+    print("not vissible")
