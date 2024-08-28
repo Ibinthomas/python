626ebde5b1ebfb377d6323128b9a72038c9f5374
Ibinthomas <ibinthomas858@gmail.com>
2024-08-28 13:22:37 +0530

    list

---
+++ list.py
@@ -0,0 +1,68 @@
+# l=[10,11,2,55,80]
+# for i in l:
+#     print(i)
+    
+# l=[10,11,2,55,80]
+# if 11 in l:
+#     print("yes")
+# else:
+#     print("no")
+"""
+yes
+"""
+
+# l=[10,22,55,88,21]
+# print(l[2])
+# l[2]="ibin"
+# print(l)
+"""
+[10, 22, 'ibin', 88, 21]
+"""
+
+# l=[10,2,'anu',69,88]
+# l.append("add")
+# print(l)
+# l.extend(['a','b','c'])
+# print(l)
+# l.insert(0,"insert")
+# print(l)
+
+"""
+[10, 2, 'anu', 69, 88, 'add']
+[10, 2, 'anu', 69, 88, 'add', 'a', 'b', 'c']
+['insert', 10, 2, 'anu', 69, 88, 'add', 'a', 'b', 'c']
+"""
+
+# l=[10,2,'anu',69,88]
+# l.pop()
+# print(l)
+"""
+[10, 2, 'anu', 69]
+"""
+# l.pop(2)
+# print(l)
+
+"""
+[10, 2, 69]
+"""
+# l.remove(69)
+# print(l)
+"""
+[10, 2]
+"""
+# l=[45,96,42,63,87]
+# l.sort()
+# print(l)
+"""
+[42, 45, 63, 87, 96]
+"""
+# l.reverse()
+# print(l)
+"""
+[96, 87, 63, 45, 42]
+"""
+# print(l.index(42))
+"""
+2
+"""
+
