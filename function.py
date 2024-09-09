735bbd0344afddaf8d427ffde864541b097548f9
Ibinthomas <ibinthomas858@gmail.com>
2024-09-09 13:26:44 +0530

    function update

--- function.py
+++ function.py
@@ -9,11 +9,32 @@
 # print("*"*10)
 # fun()
 
-def fun1():
-    a=10
-    print('a inside',a)
-    print('b=',b)
+# def fun1():
+#     b=10
+#     print('b=',b)
 
-b=20
-fun1()
-print(b)
+# b=20
+# fun1()
+# print(b)
+
+# def fun2():
+#     global a
+#     a=10
+#     print(a)
+# print("local into globel",a)
+
+# def fun3():
+#     return 'welcome',10
+# a,b=fun3()
+# print(a)
+# print(b)
+
+def fun4():
+    a=1
+    b=2
+    c=3
+    return a,b,c
+a1,b1,c1=fun4()
+print(a1)
+print(b1)
+print(c1)
