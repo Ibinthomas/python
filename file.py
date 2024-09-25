ebb20ae2c3990deddbb73b6ee8d33197568bc6ad
Ibinthomas <ibinthomas858@gmail.com>
2024-09-25 13:33:42 +0530

    file update

--- file.py
+++ file.py
@@ -36,8 +36,34 @@
 #     a=f.readline().strip()
 #     print(a[::-1])
 
-f=open('python/ibin2.txt','w')
-f.write('new file is updated')
+f=open('python/ibin2.txt','r')
+l=f.readline()
+f.seek(0)
+letter=0
+cap=0
+word=0
+# for i  in range(len(l)):
+#     a=f.readline().strip()
+#     for i in a:
+#         if i !=' ':
+#             letter+=1
+# print(letter)
 
- 
-
+for i  in range(len(l)):
+    a=f.readline().strip()
+    s=a.split(' ')
+    for i in s:
+        if i!='':
+            word+=1
+    for i in a:
+        if i!=' ':
+            if i.isupper():
+                cap+=1
+    for i in a:
+        l= l.strip()
+    letter+=1
+print(letter)
+print('cap',cap)
+print('small',letter-cap)
+print("total word",word)
+print('no of lines',len(l))
--- multiplication.py
+++ multiplication.py
@@ -1,5 +1,11 @@
 f=open('python/ibin2.txt','a')
 a=int(input("Enter the first no"))
-i=1
-for i in range(i,11):
-    f.write(str(i)+'*'+str(a)+'='+str(i*a)+'\n')
+# i=1
+# for i in range(i,11):
+#     f.write(str(i)+'*'+str(a)+'='+str(i*a)+'\n')
+
+for i in range(1,11):
+    for j in range(1,a+1):
+        f.write(str(i)+ '*'+str(j)+' = '+str(j*i) + '\n')
+    f.write('\n')
+
